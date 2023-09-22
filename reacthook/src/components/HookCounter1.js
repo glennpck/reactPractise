dd7a396871ca489c165f08949b645099e384bf37
glennpck <glennchia43@gmail.com>
2023-09-22 11:28:10 +0800

    conditionally run effects

--- reacthook/src/components/HookCounter1.js
+++ reacthook/src/components/HookCounter1.js
@@ -3,13 +3,16 @@ import React, {useState, useEffect} from 'react'
 function HookCounter1() {
 
     const [count, setCount] = useState(0)
+    const [name, setName] = useState('')
 
     useEffect(() => {
+        console.log('useEffect - Updating document title')
         document.title = `Clicked ${count} times`
-    })
+    }, [count])
 
   return (
     <div>
+        <input type='text' value={name} onChange={e => setName(e.target.value)}></input> 
       <button onClick={() => setCount(count + 1)}>Click {count} times</button>
     </div>
   )
