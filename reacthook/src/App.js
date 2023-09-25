2b84e98451e6d9eb7d6c8b8f7742c9cb7886cc02
glennpck <glennchia43@gmail.com>
2023-09-25 11:20:05 +0800

    hook context

--- reacthook/src/App.js
+++ reacthook/src/App.js
@@ -10,11 +10,22 @@ import HookMouse from './components/HookMouse';
 import MouseContainer from './components/MouseContainer';
 import IntervalHookCounter from './components/IntervalHookCounter';
 import DataFetching from './components/DataFetching';
+import ComponentC from './components/ComponentC';
+
+
+export const UserContext = React.createContext()
+export const ChannelContext = React.createContext()
 
 function App() {
   return (
     <div className="App">
-      <DataFetching />
+      <UserContext.Provider value={'nnelg'}>
+        <ChannelContext.Provider value={'pp'}>
+          <ComponentC />
+        </ChannelContext.Provider>
+      </UserContext.Provider>
+      
+      {/* <DataFetching /> */}
       {/* <IntervalHookCounter /> */}
       {/* <MouseContainer /> */}
       {/* <HookMouse /> */}
