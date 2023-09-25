4ba944d0238c7a56c66b41d2ff242156d1add2cf
glennpck <glennchia43@gmail.com>
2023-09-25 11:05:44 +0800

    hooks fetch single data

--- reacthook/src/components/DataFetching.js
+++ reacthook/src/components/DataFetching.js
@@ -2,25 +2,34 @@ import React, {useState, useEffect} from 'react'
 import axios from 'axios'
 
 function DataFetching() {
-    const [posts, setPosts] = useState([])
+    const [post, setPost] = useState({})
+    const [id, setId] = useState(1)
+    const [idFromButtonClick, setIdFromButtonClick] = useState(1)
+
+    const handleClick = () => {
+        setIdFromButtonClick(id)
+    }
 
     useEffect(() => {
-        axios.get('https://jsonplaceholder.typicode.com/posts')
+        axios.get(`https://jsonplaceholder.typicode.com/posts/${idFromButtonClick}`)
         .then(res => {
             console.log(res)
-            setPosts(res.data)
+            setPost(res.data)
         })
         .catch(err => {
             console.log(err)
         })
-    }, [])
+    }, [idFromButtonClick])
   return (
     <div>
-      <ul>
+        <input type='text' value={id} onChange={e => setId(e.target.value)} />
+        <button type='button' onClick={handleClick}>Fetch Post</button>
+      {/* <ul>
         {
             posts.map(post => <li key={post.id}>{post.title}</li>)
         }
-      </ul>
+      </ul> */}
+      <div>{post.title}</div>
     </div>
   )
 }
