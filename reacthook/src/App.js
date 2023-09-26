0f98e68c00bab6770200182a0f660f35ee4ef041
glennpck <glennchia43@gmail.com>
2023-09-26 10:26:29 +0800

    useReducer + useContext

--- reacthook/src/App.js
+++ reacthook/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useReducer} from 'react'
 import './App.css';
 import ClassCounter from './components/ClassCounter';
 import HookCounter from './components/HookCounter';
@@ -13,15 +13,41 @@ import DataFetching from './components/DataFetching';
 import ComponentC from './components/ComponentC';
 import CounterOne from './components/CounterOne';
 import CounterTwo from './components/CounterTwo';
-
+import CounterThree from './components/CounterThree';
+import ComponentG from './components/ComponentG';
+import ComponentH from './components/ComponentH';
+import ComponentI from './components/ComponentI';
 
 export const UserContext = React.createContext()
 export const ChannelContext = React.createContext()
 
+export const CountContext = React.createContext()
+
+const initialState = 0
+const reducer = (state, action) => {
+    switch(action) {
+        case 'increment':
+            return state + 1
+        case 'decrement':
+            return state - 1
+        case 'reset':
+            return initialState
+        default:
+            return state
+    }
+}
+
 function App() {
+  const [count, dispatch] = useReducer(reducer, initialState)
   return (
+    <CountContext.Provider value={{countState: count, countDispatch: dispatch}}>
     <div className="App">
-      <CounterTwo />
+      Count - {count}
+      <ComponentG />
+      <ComponentH />
+      <ComponentI />
+      {/* <CounterThree /> */}
+      {/* <CounterTwo /> */}
       {/* <CounterOne /> */}
       {/* <UserContext.Provider value={'nnelg'}>
         <ChannelContext.Provider value={'pp'}>
@@ -40,6 +66,7 @@ function App() {
       {/* <HookCounter /> */}
       {/* <ClassCounter /> */}
     </div>
+    </CountContext.Provider>
   );
 }
 
