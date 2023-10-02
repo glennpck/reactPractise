052219454f8d9562be36af586f4afa0c70bc75d8
glennpck <glennchia43@gmail.com>
2023-10-02 12:21:36 +0800

    parent+child render optimization

--- reactrender/src/App.js
+++ reactrender/src/App.js
@@ -5,11 +5,17 @@ import UseReducer from './components/useReducer/UseReducer';
 import ObjectUseState from './components/ImmutableState/ObjectUseState';
 import ArrayUseState from './components/ImmutableState/ArrayUseState';
 import Parent from './components/ParentChild/Parent';
+import ParentOne from './components/Optimization/ParentOne';
+import ChildOne from './components/Optimization/ChildOne';
+import Grandparent from './components/Optimization/Grandparent';
 
 function App() {
   return (
     <div className="App">
-      <Parent />
+      <Grandparent />
+      {/* <ParentOne>
+        <ChildOne />
+      </ParentOne> */}
       {/* <ArrayUseState /> */}
       {/* <ObjectUseState /> */}
       {/* <UseState /> */}
